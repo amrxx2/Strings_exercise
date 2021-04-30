326e147e42c54e29f27b915f9b4b3caf0df56fdc
xAmrxxx <amrmoneer21@gmail.com>
2021-04-30 20:50:43 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+public static void main(String args[]) 
+{
+String  name = "Amr Ali Moneer Mosad";
+
+for(String s: name.split(" ")){
+    //System.out.print("hellllllloooooooooow");
+    System.out.print(s.charAt(0));
+}
+    System.out.println();
+}
+}
